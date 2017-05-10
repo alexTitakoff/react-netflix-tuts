0932d8f716d9adad48d585402a91cff70ef3acb4
alex <alex@admins-MacBook-Air.local>
2017-05-10 13:26:21 +0300

    search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,10 +5,13 @@ import {
   StyleSheet,
   Image,
   TouchableWithoutFeedback,
-  TextInput
+  TextInput,
+  Dimensions
 } from 'react-native'
 
+import Icon from 'react-native-vector-icons/FontAwesome'
 
+const {width, height} = Dimensions.get('window')
 
 const shows_first = [
   {
@@ -45,27 +48,79 @@ const shows_first = [
 
 
 class Search extends  Component {
-  constructor
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
   render(){
     return (
-      <View>
-        <TextInput
-          value={this.state.text}
-          onChangeText={(text)=> this.filter(text)}
-          style={styles.input}
-          placeholder="Search"
-          placeholderTextColor="grey"
-        />
+      <View style={styles.container} >
+        <View style={styles.header}>
+          <Icon
+            name = "search"
+            color = "grey"
+            size = {18}
+            style = {styles.searchIcon}
+          />
+          <TextInput
+            value={this.state.text}
+            onChangeText={(text)=> this.filter(text)}
+            style={styles.input}
+            placeholder="Search"
+            placeholderTextColor="grey"
+          />
 
-        <TouchableWithoutFeedback>
-          <View>
-            <Text>Cancel</Text>
-          </View>
-        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback style={styles.cancelButton} >
+            <View style={styles.containerButton} >
+              <Text style={styles.cancelButtonText} >Cancel</Text>
+            </View>
+          </TouchableWithoutFeedback>
 
-      </View>
+        </View>
+    </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181818'
+  },
+  header: {
+    height: 40,
+    backgroundColor: '#181818',
+    borderBottomWidth: 1,
+    borderColor: '#3a3a3a',
+    paddingBottom: 5,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative'
+
+  },
+  searchIcon: {
+    position: 'absolute',
+    top: 5,
+    left: 15,
+    zIndex: 1,
+    backgroundColor: 'transparent'
+  },
+  input: {
+    width: width - (width/4),
+    height: 30,
+    backgroundColor: '#323232',
+    marginHorizontal: 10,
+    paddingLeft: 30,
+    borderRadius: 3,
+  },
+  cancelButtonText: {
+    color: 'white',
+
+  }
+
+})
 
 export default Search
