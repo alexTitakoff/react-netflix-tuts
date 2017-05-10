7459b4dc7dcc14328e7a553670c358a2dce5babb
alex <alex@admins-MacBook-Air.local>
2017-05-10 12:42:12 +0300

    middle result

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,13 +3,67 @@ import {
   Text,
   View,
   StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  TextInput
 } from 'react-native'
 
 
+
+const shows_first = [
+  {
+    "key":1,
+    "name":"Suits",
+    "image":"https://static.tvmaze.com/uploads/images/medium_portrait/0/2432.jpg"
+  },
+  {
+     "key":2,
+     "name":"Modern Family",
+     "image":"https://static.tvmaze.com/uploads/images/medium_portrait/0/628.jpg"
+  },
+  {
+     "key":3,
+     "name":"The Flash",
+     "image":"https://static.tvmaze.com/uploads/images/medium_portrait/78/195988.jpg"
+  },
+  {
+     "key":4,
+     "name":"Supergirl",
+     "image":"https://static.tvmaze.com/uploads/images/medium_portrait/83/209955.jpg"
+  },
+  {
+     "key":5,
+     "name":"Designated Survivor",
+     "image":"https://static.tvmaze.com/uploads/images/medium_portrait/101/253490.jpg"
+   },
+   {
+     "key":6,
+     "name":"24: Legacy",
+     "image":"https://static.tvmaze.com/uploads/images/medium_portrait/90/225030.jpg"
+  }
+]
+
+
 class Search extends  Component {
+  constructor
   render(){
     return (
-      <Text>Search</Text>
+      <View>
+        <TextInput
+          value={this.state.text}
+          onChangeText={(text)=> this.filter(text)}
+          style={styles.input}
+          placeholder="Search"
+          placeholderTextColor="grey"
+        />
+
+        <TouchableWithoutFeedback>
+          <View>
+            <Text>Cancel</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+      </View>
     )
   }
 }
